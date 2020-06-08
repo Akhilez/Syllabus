6a51e10d1d6ce14e8e6b7e660933a30b777d6f86
Akhilez <lightrescuer@gmail.com>
2020-06-07 23:28:33 -0500

    Fixed some preprocessing errors

--- syllabus_web/scrape_syllabus.py
+++ syllabus_web/scrape_syllabus.py
@@ -1,32 +1,34 @@
 from bs4 import BeautifulSoup
 import requests
 
-must_download = True
+must_download = False
 
 url = 'https://jntuh.ac.in/syllabus'
 results_length = 20  # Number of links per page
-max_pages = 2
+max_pages = 50
 post_data = {'ajax': 1, 'searchin': 'title'}
-key_words = ['M.Tech', 'Syllabus']
+key_words = ['b.tech', 'syllab']
 
 pdf_links = []
 
 for i in range(0, max_pages * results_length, results_length):
-    print(f'Page: {i / results_length}')
+    print(f'Page: {i / results_length + 1}')
 
     post_data['offset'] = i
     html_data = BeautifulSoup(requests.post(url, post_data).text, "html.parser")
 
     hyperlinks = [link for link in html_data.find_all('a') if '.pdf' in link.get('href')]
+    for link in hyperlinks:
+        link_text = link.get_text().lower()
+        if all([key_word in link_text for key_word in key_words]):
+            pdf_links.append(link.get('href'))
+            print(link_text)
+
     if len(hyperlinks) == 0:
         print(f'Last page: {i / results_length}')
         break
 
-    for link in hyperlinks:
-        href = link.get('href')
-        if all([key_word in href for key_word in key_words]):
-            pdf_links.append(href)
-            print(href)
+print(f'Total PDFs found: {len(pdf_links)}')
 
 
 def download_pdf(link):
@@ -41,3 +43,4 @@ if must_download:
     for link in pdf_links:
         print(f"Downloading PDF from: {link}")
         threading.Thread(target=download_pdf, args=(link,)).start()
+
